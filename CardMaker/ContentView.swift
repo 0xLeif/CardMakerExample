2b08ff03ac4903d63cb9fd8113aa8cc527d01e57
zmeriksen <zmeriksen@gmail.com>
2019-07-25 17:16:25 -0500

    Initial Commit

---
+++ CardMaker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CardMaker
+//
+//  Created by Zach Eriksen on 7/25/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
